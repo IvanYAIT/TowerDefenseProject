755869bb0c31ad38bc09ffb76fa0da49a02f0999
IvanYAIT <83347588+IvanYAIT@users.noreply.github.com>
2022-12-02 15:07:25 +0300

    fix enemy movemnt 2

--- Assets/Scripts/MyDirectory/DragObjects.cs
+++ Assets/Scripts/MyDirectory/DragObjects.cs
@@ -6,15 +6,23 @@ using UnityEngine.EventSystems;
 public class DragObjects : MonoBehaviour, IBeginDragHandler, IEndDragHandler, IDragHandler
 {
     [SerializeField] GameObject TowerPrefab;
+    [SerializeField] Transform parent;
+
+    private Vector3 startPosition;
+
+    private void Start()
+    {
+        startPosition = transform.position;
+    }
 
     public void OnBeginDrag(PointerEventData eventData)
     {
-        
+        transform.SetParent(parent);
     }
 
     public void OnDrag(PointerEventData eventData)
     {
-        transform.position = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, 1, Input.mousePosition.z));
+        transform.position = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 1));
     }
 
     public void OnEndDrag(PointerEventData eventData)
@@ -26,12 +34,14 @@ public class DragObjects : MonoBehaviour, IBeginDragHandler, IEndDragHandler, ID
         {
             if(hit.transform.gameObject.tag == "towerPos")
             {
-                Instantiate(TowerPrefab, new Vector3(hit.transform.position.x, hit.transform.position.y, hit.transform.position.z), transform.rotation);
+                Instantiate(TowerPrefab, new Vector3(hit.transform.position.x-1.1f, hit.transform.position.y+1.5f, hit.transform.position.z-1.1f), new Quaternion(1,0,0,-45));
+                
             }
             else
             {
                 Debug.Log("????????????????????????????????????????????????????????????????????????????????????????????????????.?.?,??.??????????????????????????????????????");
             }
+            transform.position = startPosition;
         }
     }
 
