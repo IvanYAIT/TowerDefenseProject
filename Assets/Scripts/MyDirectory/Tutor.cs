d25beacaa8b2b4e98501013cbc895a7eba537995
tearsofdespair <76656828+tearsofdespair@users.noreply.github.com>
2022-12-04 14:58:10 +0300

    upgrade tutor

--- Assets/Scripts/MyDirectory/Tutor.cs
+++ Assets/Scripts/MyDirectory/Tutor.cs
@@ -1,19 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class Tutor : MonoBehaviour
 {
     [Tooltip("objects that will be shown")]
-    [SerializeField] GameObject[] gameObjects;
+    [SerializeField] Image[] sprites;
+    
     private int i = 0;
+
+    
+
+    IEnumerator LerpShow(Image sprite)
+    {
+        Color c = sprite.color;
+        for (float f = 0.05f;f <=1;f+= 0.05f)
+        {
+            
+            c.a = f;
+            sprite.color = c;
+            yield return new WaitForSeconds(0.05f);
+        }
+        c.a = 1;
+        sprite.color = c;
+    }
+
 
     public void Show()
     {
-        if(i < gameObjects.Length)
+        if(i < sprites.Length)
         {
-            gameObjects[i].SetActive(true);
+
+            StartCoroutine(LerpShow(sprites[i]));
+            i++;
         }
         else
         {
